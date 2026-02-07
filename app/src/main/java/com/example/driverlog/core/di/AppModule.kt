7a6a5ed7ad10fbad8282f69046ca7921c17da9ed
Sunil Rana <sunil.rana320778@gmail.com>
2026-02-07 20:52:34 +0530

    feat: Integrate Room database

--- app/src/main/java/com/example/driverlog/core/di/AppModule.kt
+++ app/src/main/java/com/example/driverlog/core/di/AppModule.kt
@@ -1,9 +1,13 @@
 package com.example.driverlog.core.di
 
+import android.app.Application
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.room.Room
 import com.example.driverlog.core.utils.dataStore
+import com.example.driverlog.data.local.AppDatabase
+import com.example.driverlog.data.local.dao.TransactionDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,4 +23,19 @@ object AppModule {
     fun provideDataStore(@ApplicationContext context: Context) : DataStore<Preferences>{
         return context.dataStore
     }
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application) : AppDatabase{
+        return Room.databaseBuilder(
+            app,
+            AppDatabase::class.java,
+            "app_database"
+        ).build()
+    }
+
+    @Provides
+    fun provideTransactionDao(db: AppDatabase): TransactionDao = db.getTransactionDao()
+
+
 }
